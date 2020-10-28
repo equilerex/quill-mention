b7e2828774fb3f0c373560dd01658b240f386912
joosep <jokoivi@gmail.com>
2020-10-28 15:13:50 +0100

    [android bug work-around] - Android's backspace works a bit different from other keyboards - the event does not get fired properly and the cursor will break into the "read only" blob where it will wreck havoc by semi-breaking the editor. (will not be able to remove the blob, any sequential changes will make it copy the blob text)

--- src/blots/mention.js
+++ src/blots/mention.js
@@ -5,15 +5,38 @@ const Embed = Quill.import("blots/embed");
 class MentionBlot extends Embed {
   static create(data) {
     const node = super.create();
+    // prefix character
     const denotationChar = document.createElement("span");
     denotationChar.className = "ql-mention-denotation-char";
     denotationChar.innerHTML = data.denotationChar;
+    denotationChar.setAttribute("contenteditable", false);
+
+    // Content
+    const dataContainer = document.createElement("span");
+    dataContainer.innerHTML = data.value;
+    dataContainer.setAttribute("contenteditable", false);
+
+    // when android keyboard reaches a `contenteditable=false` block, it automatically closes.
+    // avoid that by adding a buffer "space" without the attribute.
+    const AndroidBackspaceFix = document.createElement("span");
+    AndroidBackspaceFix.innerHTML = "&nbsp;";
+    // it needs to be "visible" in order to work - so limit to minimal size.
+    AndroidBackspaceFix.setAttribute("style", "display: inline-block; height: 1px; width: 1px; overflow: hidden; ");
+
     node.appendChild(denotationChar);
-    node.innerHTML += data.value;
+    node.appendChild(dataContainer);
+    node.appendChild(AndroidBackspaceFix);
+
     return MentionBlot.setDataValues(node, data);
   }
 
   static setDataValues(element, data) {
+    // the extended Embed constructor has added contenteditable=false to the outermost span,
+    // we want to override that in favour of ones applied to the child elements inside create()
+    setTimeout(() => {
+      element.getElementsByTagName("span")[0].setAttribute("contenteditable", "inherit");
+    }, 0);
+
     const domNode = element;
     Object.keys(data).forEach(key => {
       domNode.dataset[key] = data[key];
@@ -23,6 +46,22 @@ class MentionBlot extends Embed {
 
   static value(domNode) {
     return domNode.dataset;
+  }
+
+  // android Gboard backspace does not fire onkeypress events, resulting in the caret
+  // breaking into the read-only blot element. - so we need to handle edit events inside the blot child elements as well
+  update(mutations, context) {
+    // `childList` mutations are not handled on Quill
+    // see `update` implementation on:
+    // https://github.com/quilljs/quill/blob/master/blots/embed.js
+
+    // any attempt at modifying the inner content will just remove it
+    // (since we cant block any modifiications completely, this is the "lesser evil" / graceful fallback)
+    for (const mutation of mutations) {
+      if (mutation.type === "attributes" && mutation.attributeName === "contenteditable") continue;
+      setTimeout(() => this.remove(), 0);
+      return;
+    }
   }
 }
 
